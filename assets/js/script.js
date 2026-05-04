107a792415f1b0f31d6823950a8c37456a2a8b36
bikerbrandon2282 <bikerbrandon2282@gmail.com>
2026-05-04 13:04:00 +0100

    Display Addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,15 +6,16 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
+                let gameType = this.getAttribute("data-type");
                 alert("You clicked submit!");
             }
             else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
-                //runGame(gameType);
+                runGame(gameType);
             }
         });
     }
+    runGame("addition");
 });
 /**
  * The main game "loop", called when the script is first loaded
@@ -24,6 +25,23 @@ function runGame(gameType) {
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    }
+    else if (gameType === "subtract") {
+        displaySubtractQuestion(num1, num2);
+    }
+    else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
+    }
+    else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
+    }
+    else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 function checkAnswer() {
 
@@ -37,12 +55,15 @@ function incrementScore() {
 function incrementWrongAnswer() {
 
 }
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 
 }
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
 
 }
-function displayMultiplyQuestion() {
+function displayMultiplyQuestion(operand1, operand2) {
 
 }
